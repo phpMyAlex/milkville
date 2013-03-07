9e37ef1a3f9ca48cfbf1b5294cd2736bd4ddf087
Alex Fuller <afuller101@gmail.com>
2013-03-07 18:15:34 +0000

    Page

---
+++ page.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="author" content="">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<link href="style.css" rel="stylesheet" type="text/css">
+	<link href="images/favicon.png" rel="shortcut icon" type="image/png">
+	<title>Milk Ville - Page</title>
+</head>
+
+<body>
+	<img src="images/logo.png" width="140" height="140" id="logo" alt="Logo">
+	<div id="container">
+		<div id="posthold">
+			<div class="thumb">
+				<h1>Welcome</h1>
+				<div class="white"></div>
+				<img src="images/thumb1.png" alt="test">
+			</div>
+			<div class="content">
+				<p>Lorem ipsum dolor sit amet, et eam veniam mnesarchum assueverit, vero consetetur vis in. Diam timeam persius an eam. Te nam omnesque laboramus. Te tota scribentur vix, per ea scripta eloquentiam. Eius option splendide cu vel. No duo nihil percipit assentior, ferri labore sanctus ut nam, reque ponderum dignissim quo at. Oratio feugait electram eum in, vis at everti offendit pericula.</p>
+				<p>Lorem ipsum dolor sit amet, et eam veniam mnesarchum assueverit, vero consetetur vis in. Diam timeam persius an eam. Te nam omnesque laboramus. Te tota scribentur vix, per ea scripta eloquentiam. Eius option splendide cu vel. No duo nihil percipit assentior, ferri labore sanctus ut nam, reque ponderum dignissim quo at. Oratio feugait electram eum in, vis at everti offendit pericula.</p>
+				<p>Lorem ipsum dolor sit amet, et eam veniam mnesarchum assueverit, vero consetetur vis in. Diam timeam persius an eam. Te nam omnesque laboramus. Te tota scribentur vix, per ea scripta eloquentiam. Eius option splendide cu vel. No duo nihil percipit assentior, ferri labore sanctus ut nam, reque ponderum dignissim quo at. Oratio feugait electram eum in, vis at everti offendit pericula.</p>
+				<p>Lorem ipsum dolor sit amet, et eam veniam mnesarchum assueverit, vero consetetur vis in. Diam timeam persius an eam. Te nam omnesque laboramus. Te tota scribentur vix, per ea scripta eloquentiam. Eius option splendide cu vel. No duo nihil percipit assentior, ferri labore sanctus ut nam, reque ponderum dignissim quo at. Oratio feugait electram eum in, vis at everti offendit pericula.</p>
+			</div>
+			<div id="info"></div>
+		</div>
+	</div>
+</body>
+</html>
