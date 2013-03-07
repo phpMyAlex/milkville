4b1825f3e235d32f22ab118a2ce08e19ee252131
Alex Fuller <afuller101@gmail.com>
2013-03-07 17:31:01 +0000

    newest

--- index.php
+++ index.php
@@ -11,11 +11,56 @@
 </head>
 
 <body>
+	<img src="images/logo.png" width="140" height="140" id="logo" alt="Logo">
 	<div id="container">
-		<section class="left">
-			<img src="images/logo.png" width="220" height="220" alt="Logo">
-		</section>
-		<section class="right"></section>
+		<div id="posthold">
+			<div class="thumb">
+				<h1>Welcome</h1>
+				<div class="white"></div>
+				<img src="images/thumb1.png" alt="test">
+			</div>
+			<div class="content">
+				<p>Lorem ipsum dolor sit amet, et eam veniam mnesarchum assueverit, vero consetetur vis in. Diam timeam persius an eam. Te nam omnesque laboramus. Te tota scribentur vix, per ea scripta eloquentiam. Eius option splendide cu vel. No duo nihil percipit assentior, ferri labore sanctus ut nam, reque ponderum dignissim quo at. Oratio feugait electram eum in, vis at everti offendit pericula.</p>
+				<div id="readmore">Read More</div>
+			</div>
+		</div>
+		<div id="posthold">
+			<div class="thumb">
+				<h1>Welcome</h1>
+				<div class="white"></div>
+				<img src="images/thumb1.png" alt="test">
+			</div>
+			<div class="content">
+				<p>Lorem ipsum dolor sit amet, et eam veniam mnesarchum assueverit, vero consetetur vis in. Diam timeam persius an eam. Te nam omnesque laboramus. Te tota scribentur vix, per ea scripta eloquentiam. Eius option splendide cu vel. No duo nihil percipit assentior, ferri labore sanctus ut nam, reque ponderum dignissim quo at. Oratio feugait electram eum in, vis at everti offendit pericula.</p>
+				<div id="readmore">Read More</div>
+			</div>
+		</div>
+		<div id="posthold">
+			<div class="thumb">
+				<h1>Welcome</h1>
+				<div class="white"></div>
+				<img src="images/thumb1.png" alt="test">
+			</div>
+			<div class="content">
+				<p>Lorem ipsum dolor sit amet, et eam veniam mnesarchum assueverit, vero consetetur vis in. Diam timeam persius an eam. Te nam omnesque laboramus. Te tota scribentur vix, per ea scripta eloquentiam. Eius option splendide cu vel. No duo nihil percipit assentior, ferri labore sanctus ut nam, reque ponderum dignissim quo at. Oratio feugait electram eum in, vis at everti offendit pericula.</p>
+				<div id="readmore">Read More</div>
+			</div>
+		</div>
+		<div id="posthold">
+			<div class="thumb">
+				<h1>Welcome</h1>
+				<div class="white"></div>
+				<img src="images/thumb1.png" alt="test">
+			</div>
+			<div class="content">
+				<p>Lorem ipsum dolor sit amet, et eam veniam mnesarchum assueverit, vero consetetur vis in. Diam timeam persius an eam. Te nam omnesque laboramus. Te tota scribentur vix, per ea scripta eloquentiam. Eius option splendide cu vel. No duo nihil percipit assentior, ferri labore sanctus ut nam, reque ponderum dignissim quo at. Oratio feugait electram eum in, vis at everti offendit pericula.</p>
+				<div id="readmore">Read More</div>
+			</div>
+		</div>
+		<div id="nextprev">
+			<div class="next">Next</div>
+			<div class="prev">Previous</div>
+		</div>
 	</div>
 </body>
 </html>
