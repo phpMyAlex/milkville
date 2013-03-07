c30e1d49d7b498c4287de29667223da44e4ddc9b
Alex Fuller <afuller101@gmail.com>
2013-03-07 14:43:23 +0000

    Latest

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="author" content="">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<link href="style.css" rel="stylesheet" type="text/css">
+	<link href="images/favicon.png" rel="shortcut icon" type="image/png">
+	<title>Milk Ville</title>
+</head>
+
+<body>
+	<div id="container">
+		<section class="left">
+			<img src="images/logo.png" width="220" height="220" alt="Logo">
+		</section>
+		<section class="right"></section>
+	</div>
+</body>
+</html>
